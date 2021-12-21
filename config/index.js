d63a6686465e3032ac0b6c2f70fb7a07c13d94d1
Irvan Indriyan <irvanindriyan@gmail.com>
2021-12-21 12:16:52 +0700

    Udate

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@ require("dotenv").config()
 const conf = {};
 conf.environment = process.env.ENV;
 conf.sequelize = {};
-conf.sequelize.username = process.env.DB_USER
-conf.sequelize.password = process.env.DB_PASS
-conf.sequelize.database = process.env.DB_NAME
+conf.sequelize.username = process.env.DB_USERNAME
+conf.sequelize.password = process.env.DB_PASSWORD
+conf.sequelize.database = process.env.DB_DATABASE
 conf.sequelize.host = process.env.DB_HOST
-conf.sequelize.dialect = 'mysql';
-conf.sequelize.port = 3306;
+conf.sequelize.dialect = process.env.DB_CONNECTION;
+conf.sequelize.port = process.env.DB_PORT;
 conf.sequelize.define = {
   charset: 'utf8mb4', 
     dialectOptions: {
