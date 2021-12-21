e90bf38f23a613bda7b7612fcc4db349ffe4d943
Irvan Indriyan <irvanindriyan@gmail.com>
2021-12-21 12:19:58 +0700

    Udate

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
-'use strict'
-const express = require('express')
-const auth = require('./authRoutes')
-const fetchData = require('./fetchRoutes')
-const router = express()
+'use strict';
+const express = require('express');
+const auth = require('./authRoutes');
+const fetchData = require('./fetchRoutes');
+const router = express();
+
+router.get(`/`, (_req, res) => {
+  res.json({
+    "message": "Welcome to Rest Api"
+  });
+});
 
 router.get(`/api/`, (_req, res) => {
   res.json({
     "message": "Welcome to Rest Api"
-  })
-})
+  });
+});
 
-router.use(auth)
-router.use(fetchData)
+router.use(auth);
+router.use(fetchData);
 
-module.exports = router
+module.exports = router;
